d8837d7915c8290a69f10de1b5d6eeadc8e3813f
Arthur Simões Gonzaga <arthursgonzaga@gmail.com>
2023-08-14 20:39:56 -0300

    adjusting loader

--- jobs/base/loader.py
+++ jobs/base/loader.py
@@ -1,33 +1,35 @@
-import os
+import awswrangler as wr
 import boto3
-from dotenv import load_dotenv
+import os
 
-class S3Uploader:
-    def __init__(self, bucket_name):
-        # Inicialização do cliente S3 e nome do bucket.
-        # Lê as credenciais da AWS do arquivo .env
-        load_dotenv()
-        aws_access_key_id = os.getenv('AWS_ACCESS_KEY_ID')
-        aws_secret_access_key = os.getenv('AWS_SECRET_ACCESS_KEY')
+class S3Loader:
+    def __init__(self):
+        aws_access_key_id = os.getenv("aws_access_key_id")
+        aws_secret_access_key = os.getenv("aws_secret_access_key")
+        self.session = boto3.session.Session(aws_access_key_id=aws_access_key_id,
+                                        aws_secret_access_key=aws_secret_access_key, 
+                                        region_name='us-east-2')
+        
+    def create_database(self, database_name):
+        # TO-DO: criar os logs e checar erro
+        wr.catalog.create_database(database_name, exist_ok=True, boto3_session=self.session)
 
-        self.s3_client = boto3.client(
-                                's3', 
-                                aws_access_key_id=aws_access_key_id, 
-                                aws_secret_access_key=aws_secret_access_key
-                            )
-        self.bucket_name = bucket_name
-  
+    def load_data(self, data, path, layer, database_name, table_name):
+        # TO-DO: criar os logs e checar erro
+        if layer == 'raw':
+            res = wr.s3.to_csv(
+                df=data,
+                sep='|',
+                path=path,
+                dataset=True,
+                mode="overwrite",
+                database=database_name,
+                table=table_name,
+                boto3_session=self.session
+            )
+        else:
+            # TO-DO: criar para próximas camadas
+            pass
+            
 
-    def upload_file(self, local_file_path, s3_key):
-        # Faz o upload de um arquivo local para o S3.
-
-        # Verifica se o arquivo local existe.
-        if not os.path.isfile(local_file_path):
-            raise FileNotFoundError(f"O arquivo {local_file_path} não foi encontrado.")
-
-        try:
-            # Faz o upload do arquivo para o S3.
-            self.s3_client.upload_file(local_file_path, self.bucket_name, s3_key)
-            print(f"Arquivo {local_file_path} enviado com sucesso para o S3.")
-        except Exception as e:
-            print(f"Erro ao enviar o arquivo {local_file_path} para o S3: {e}")
+        return res
